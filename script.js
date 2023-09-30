d392a9c5fd2cb8d69c35134e592d443bf48be73f
YmagineMax <maksymilian.kedzierski23@gmail.com>
2023-09-30 23:04:00 +0200

    Added a navbar, css styling and simple js to open up hamburger menu

---
+++ script.js
@@ -0,0 +1,9 @@
+// Button for hamburger menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav__links')[0]
+
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active')
+})
+
