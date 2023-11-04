026f359949cb39b94be47f4692d2b61571293cb8
YmagineMax <maksymilian.kedzierski23@gmail.com>
2023-11-04 23:03:32 +0100

    Added function - when you click on the event info windows pops out

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ toggleButton.addEventListener('click', () => {
 navbarLinks.classList.toggle('active')
 })
 
+// Get the button and the section elements
+const toggleCompletedChallengesBtn = document.getElementById('cmted_chall_btn');
+const completedChallengesSection = document.getElementById('sec_cmted');
+
+// Add click event listener to the button
+toggleCompletedChallengesBtn.addEventListener('click', () => {
+  // Check the current display style of the section and toggle it
+  if (completedChallengesSection.style.display === 'none' || completedChallengesSection.style.display === '') {
+    completedChallengesSection.style.display = 'block';
+    toggleCompletedChallengesBtn.textContent = 'Schowaj zakończone wyzwania';
+  } else {
+    completedChallengesSection.style.display = 'none';
+    toggleCompletedChallengesBtn.textContent = 'Sprwadź zakończone wyzwania';
+  }
+});
+
+document.querySelectorAll('.challenge-container').forEach(container => {
+    container.addEventListener('click', (event) => {
+      // Prevent click event from affecting parent elements
+      event.stopPropagation();
+  
+      const extendedInfo = container.querySelector('.extended-info');
+      const isVisible = extendedInfo.style.display === 'block';
+      extendedInfo.style.display = isVisible ? 'none' : 'block';
+    });
+  });
+  
