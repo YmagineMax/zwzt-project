20467a50aa293f1a13f7ec1de9b6b729f5b30b7a
YmagineMax <maksymilian.kedzierski23@gmail.com>
2023-11-12 14:09:10 +0100

    Basic login modal

--- script.js
+++ script.js
@@ -76,3 +76,37 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+// Get the modal
+var modal = document.getElementById("loginModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("nav_bar_button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Switch to register
+document.getElementById("switchToRegister").addEventListener("click", function() {
+  // Here you can handle the switch to the registration form
+  // For example, hide the login form and show the registration form
+});
+
+
