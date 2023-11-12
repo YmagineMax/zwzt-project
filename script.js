87962adb10757b03e8f10f32cce72e534f7846f4
YmagineMax <maksymilian.kedzierski23@gmail.com>
2023-11-12 18:12:55 +0100

    Finished a forms for login and register

--- script.js
+++ script.js
@@ -119,4 +119,11 @@ document.getElementById("switchToRegister").addEventListener("click", function()
 });
 
 
+document.getElementById('switchToLogin').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('registerForm').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'block';
+});
+
+
 
